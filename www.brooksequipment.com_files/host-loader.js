714dfb6e5e706afa8f66bdffd7b7a5a34417a3ee
Janet Walker <janetwalker271989@gmail.com>
2017-07-31 09:36:20 -0400

    another push

--- www.brooksequipment.com_files/host-loader.js
+++
@@ -1,3 +0,0 @@
-(function(){if(window.mfesecure_loaded){return}window.mfesecure_loaded=true;try{var a=document.createElement("script");a.setAttribute("type","text/javascript");
-a.setAttribute("src","//cdn.ywxi.net/js/host.js?v=20170725225023123973&h=brooksequipment.com");document.getElementsByTagName("head")[0].appendChild(a)
-}catch(b){}})();
